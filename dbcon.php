a1a91a15afe0b9563e68c4c908cbfd024ffaef9b
Tanvi Shetty <56154967+tanvi0915@users.noreply.github.com>
2021-07-20 18:25:37 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,12 +1,23 @@
 <?php
+//Development connection
+// $server = "localhost";
+// $user = "root";
+// $password ="abc456";
+// $db = "signup";
 
-$server = "localhost";
-$user = "root";
-$password ="abc456";
-$db = "signup";
+
+// $con=mysqli_connect($server,$user,$password,$db,"8111");
 
 
-$con=mysqli_connect($server,$user,$password,$db,"8111");
+
+$server = "sql6.freemysqlhosting.net";
+$user = "sql6426667";
+$password ="rCfzPswVVS";
+$db = "sql6426667";
+
+
+$con=mysqli_connect($server,$user,$password,$db,"3306"); // for development connection you need to change port to 8111
+
 
 if($con){
 	?>
